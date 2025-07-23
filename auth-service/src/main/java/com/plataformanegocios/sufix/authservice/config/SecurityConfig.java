af37f486cef53e58434c8d06870fb92957e49164
allandesenv <allandd8@gmail.com>
2025-07-22 21:26:31 -0300

    auth-service Integracao JWT

---
+++ auth-service/src/main/java/com/plataformanegocios/sufix/authservice/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.plataformanegocios.sufix.authservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // Desabilita CSRF para APIs REST sem sessão
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll() // Permite todas as requisições por enquanto
+                );
+        return http.build();
+    }
+}
